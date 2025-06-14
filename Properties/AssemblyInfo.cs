1909e9505b8743b33b35d5eebc64c6d8b2e00763
wristcry <cryingkiddo@gmail.com>
2025-06-14 16:51:30 +0300

    AAAAA

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Interception.DiscordSpy")]
+[assembly: AssemblyTitle("Interception.DiscordStartAnnouncement")]
 [assembly: AssemblyDescription("rocket plugin")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("vk.com/interceptionplugins")]
